e1b6431277272dcd880135e5c4e6f3b227fa50f3
davquipe <davidcondori99@gmail.com>
2020-01-29 18:35:50 -0500

    Trabajando con la Interfaz de Tareas

---
+++ src/components/tareas/ListadoTareas.js
@@ -0,0 +1,32 @@
+import React, { Fragment } from 'react';
+import Tarea from './Tarea';
+
+const ListadoTareas = () => {
+
+    const tareasProyectos = [
+        { nombre: 'Eligir Plataforma', estado: true },
+        { nombre: 'Eligir Colores', estado: false },
+        { nombre: 'Eligir Plataforma de Pago', estado: false },
+        { nombre: 'Eligir Hosting', estado: true }
+    ]
+
+    return ( 
+        <Fragment>
+            <h2>Proyecto: Tienda Virual</h2>
+
+            <ul className="listado-tareas">
+                {tareasProyectos.length === 0 
+                    ? (<li className="tarea"><p>No hay Tareas</p></li>)
+
+                    : tareasProyectos.map(tarea => (
+                        <Tarea 
+                            tarea={tarea}
+                        />
+                    ))
+                }
+            </ul>
+        </Fragment>
+     );
+}
+ 
+export default ListadoTareas;
